b4c10caccf82716a8299dbad36dad2e0bffcffc0
Easa Yeasfi Bin Islam <oritrobinislam@gmail.com>
2025-01-12 11:51:32 +0600

    leetcode solutinon

--- pattern.cpp
+++ pattern.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void print (int n)
 {
     for( int i=0; i<n; i++){
-        for (int j=0; j<n; j++)
+        for (int j=0; j<=i; j++)
         {
             cout << " N ";
         }
